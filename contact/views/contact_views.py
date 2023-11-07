be1a9cf1bd431fa8e32a0e84b268d4ea3fbdcc43
BrookLiNN <allanpauluk@hotmail.com>
2023-11-06 21:47:09 -0300

    Criado a tabela de contatos, e a url contact para cada contato

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,9 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from contact.models import Contact
+from django.http import Http404
 
 # Create your views here.
 
 def index(request):
+    contacts = Contact.objects.filter(show=True).order_by('-id')[:10]
+
+    context = {
+        'contacts': contacts,
+    }
+
     return render(
         request,
         'contact/index.html',
+        context
+    )
+
+
+def contact(request, contact_id):
+    # single_contact = Contact.objects.filter(pk=contact_id).first()
+    single_contact = get_object_or_404(Contact.objects.filter(pk=contact_id, show=True))
+    # if single_contact is None:
+    #     raise Http404()
+
+    context = {
+        'contact': single_contact,
+    }
+
+    return render(
+        request,
+        'contact/contact.html',
+        context
     )
