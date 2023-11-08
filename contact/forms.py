5acd5dcad31b3bb563daa37d911a2a73655b5e6b
BrookLiNN <allanpauluk@hotmail.com>
2023-11-08 20:42:52 -0300

    Criando os campos do forms

--- contact/forms.py
+++ contact/forms.py
@@ -2,7 +2,18 @@ from django import forms
 from django.core.exceptions import ValidationError
 from . import models
 
+
+
 class ContactForm(forms.ModelForm):
+
+    first_name = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Escreva seu primeiro nome'}),
+                                 label='Nome', help_text='Texto de ajuda para o usuário')
+    
+    last_name = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Escreva seu sobrenome'}),
+                                 label='Sobrenome', help_text='Texto de ajuda para o usuário')    
+    phone = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Escreva seu número de telefone'}),
+                                 label='Telefone', help_text='Texto de ajuda para o usuário')
+
     class Meta:
         model = models.Contact
         fields = ('first_name', 'last_name', 'phone',)
