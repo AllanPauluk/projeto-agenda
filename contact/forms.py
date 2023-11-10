e0f4dab5410f24312518d76450b2cd222a6cb881
BrookLiNN <allanpauluk@hotmail.com>
2023-11-10 18:23:38 -0300

    Criando a validação dos erros

--- contact/forms.py
+++ contact/forms.py
@@ -18,22 +18,24 @@ class ContactForm(forms.ModelForm):
         model = models.Contact
         fields = ('first_name', 'last_name', 'phone',)
 
-    def clean(self):
-        # cleaned_data = self.cleaned_data
 
-        self.add_error(
-            'first_name',
-            ValidationError(
-                'Mensagem de erro',
-                code='invalid'
-            )
-        )        
-        self.add_error(
-            'last_name',
-            ValidationError(
-                'Mensagem de erro 2',
-                code='invalid'
-            )
-        )
+    # Valor de mais de um campo
+    def clean(self):
+        cleaned_data = self.cleaned_data
+        first_name = cleaned_data.get('first_name')
+        last_name = cleaned_data.get('last_name')
+
+        if first_name == last_name:
+            self.add_error('last_name', ValidationError('O segundo nome não pode ser igual ao primeiro', code='invalid'))
+       
 
         return super().clean()
+    
+    # Valor de apenas um campo
+    def clean_first_name(self):
+        first_name = self.cleaned_data.get('first_name')
+
+        if first_name == 'ABC':
+             self.add_error('first_name', ValidationError('Mensagem de erro', code='invalid'))   
+
+        return first_name
