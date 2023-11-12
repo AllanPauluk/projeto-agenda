67ca07c88f4372d8e07f5ea39a53f5b1048acdc6
BrookLiNN <allanpauluk@hotmail.com>
2023-11-12 14:08:50 -0300

    Criando e configurando os formulários de user update, user create e login/logout

--- contact/views/user_forms.py
+++ contact/views/user_forms.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
-from contact.forms import RegisterForm
+from django.shortcuts import render, redirect
+from contact.forms import RegisterForm, RegisterUpdateForm
+from django.contrib import messages
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib import auth
 
 def register(request):
     form = RegisterForm()
@@ -9,6 +12,8 @@ def register(request):
 
         if form.is_valid():
             form.save()
+            messages.success(request, 'Usuário criado com sucesso')
+            return redirect('contact:index')
 
     return render(
         request,
@@ -17,3 +22,57 @@ def register(request):
             'form': form,
         }
     )
+
+def user_update(request):
+    form = RegisterUpdateForm(instance=request.user)
+
+    if request.method != 'POST':
+        return render(
+            request,
+            'contact/user_update.html',
+            {
+                'form': form,
+            }
+        )
+    
+    form = RegisterUpdateForm(data=request.POST, instance=request.user)
+
+    if not form.is_valid():
+        return render(
+            request,
+            'contact/user_update.html',
+            {
+                'form': form,
+            }
+        )
+    form.save()
+    messages.success(request, 'Usuário alterado com sucesso')
+    return redirect('contact:user_update')
+
+
+def login_view(request):
+        form = AuthenticationForm(request)
+
+        if request.method == 'POST':
+            form = AuthenticationForm(request, data=request.POST)
+
+            if form.is_valid():
+                user = form.get_user()
+                auth.login(request, user)
+                messages.success(request, 'Login efetuado!')
+                return redirect('contact:index')
+            messages.error(request, 'Login inválido')
+
+        return render(
+        request,
+        'contact/login.html',
+        {
+            'form': form,
+        }
+    )
+
+def logout_view(request):
+    auth.logout(request)     
+    
+    messages.info(request, 'Deslogado com sucesso')
+    return redirect('contact:login')
